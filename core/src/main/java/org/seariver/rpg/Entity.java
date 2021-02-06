51e57b50386118c0ed84a5584c517ba9b48db120
Mario Rezende <mariorez@gmail.com>
2021-02-06 18:36:38 -0300

    implement the game foundation classes and logics

---
+++ core/src/main/java/org/seariver/rpg/Entity.java
@@ -0,0 +1,258 @@
+package org.seariver.rpg;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.UUID;
+
+public class Entity {
+
+    private static final String TAG = Entity.class.getSimpleName();
+    private static final String defaultSpritePath = "sprites/characters/Warrior.png";
+
+    private Vector2 velocity;
+    private String entityID;
+
+    private Direction currentDirection = Direction.LEFT;
+    private Direction previousDirection = Direction.UP;
+
+    private Animation walkLeftAnimation;
+    private Animation walkRightAnimation;
+    private Animation walkUpAnimation;
+    private Animation walkDownAnimation;
+
+    private Array<TextureRegion> walkLeftFrames;
+    private Array<TextureRegion> walkRightFrames;
+    private Array<TextureRegion> walkUpFrames;
+    private Array<TextureRegion> walkDownFrames;
+
+    protected Vector2 nextPlayerPosition;
+    protected Vector2 currentPlayerPosition;
+    protected State state = State.IDLE;
+    protected float frameTime = 0f;
+    protected Sprite frameSprite = null;
+    protected TextureRegion currentFrame = null;
+
+    public final int FRAME_WIDTH = 16;
+    public final int FRAME_HEIGHT = 16;
+    public static Rectangle boundingBox;
+
+    public enum State {
+        IDLE, WALKING
+    }
+
+    public enum Direction {
+        UP, RIGHT, DOWN, LEFT;
+    }
+
+    public Entity() {
+        initEntity();
+    }
+
+    public void initEntity() {
+        this.entityID = UUID.randomUUID().toString();
+        this.nextPlayerPosition = new Vector2();
+        this.currentPlayerPosition = new Vector2();
+        this.boundingBox = new Rectangle();
+        this.velocity = new Vector2(2f, 2f);
+
+        Utility.loadTextureAsset(defaultSpritePath);
+        loadDefaultSprite();
+        loadAllAnimations();
+    }
+
+    public void update(float delta) {
+
+        frameTime = (frameTime + delta) % 5; //Want to avoid overflow
+
+        // We want the hit box to be at the feet for a better feel
+        setBoundingBoxSize(0f, 0.5f);
+    }
+
+    public void init(float startX, float startY) {
+
+        this.currentPlayerPosition.x = startX;
+        this.currentPlayerPosition.y = startY;
+
+        this.nextPlayerPosition.x = startX;
+        this.nextPlayerPosition.y = startY;
+    }
+
+    public void setBoundingBoxSize(float percentageWidthReduced, float percentageHeightReduced) {
+
+        //Update the current bounding box
+        float width;
+        float height;
+
+        float widthReductionAmount = 1.0f - percentageWidthReduced; //.8f for 20% (1 - .20)
+        float heightReductionAmount = 1.0f - percentageHeightReduced; //.8f for 20% (1 - .20)
+
+        if (widthReductionAmount > 0 && widthReductionAmount < 1) {
+            width = FRAME_WIDTH * widthReductionAmount;
+        } else {
+            width = FRAME_WIDTH;
+        }
+
+        if (heightReductionAmount > 0 && heightReductionAmount < 1) {
+            height = FRAME_HEIGHT * heightReductionAmount;
+        } else {
+            height = FRAME_HEIGHT;
+        }
+
+        if (width == 0 || height == 0) {
+            Gdx.app.debug(TAG, "Width and Height are 0 !! " + width + ":" + height);
+        }
+
+        //Need to account for the unitscale, since the map coordinates will be in pixels
+        float minX;
+        float minY;
+
+        if (MapManager.UNIT_SCALE > 0) {
+            minX = nextPlayerPosition.x / MapManager.UNIT_SCALE;
+            minY = nextPlayerPosition.y / MapManager.UNIT_SCALE;
+        } else {
+            minX = nextPlayerPosition.x;
+            minY = nextPlayerPosition.y;
+        }
+
+        boundingBox.set(minX, minY, width, height);
+    }
+
+    private void loadDefaultSprite() {
+        Texture texture = Utility.getTextureAsset(defaultSpritePath);
+        TextureRegion[][] textureFrames = TextureRegion.split(texture, FRAME_WIDTH, FRAME_HEIGHT);
+        frameSprite = new Sprite(textureFrames[0][0].getTexture(), 0, 0, FRAME_WIDTH, FRAME_HEIGHT);
+        currentFrame = textureFrames[0][0];
+    }
+
+    private void loadAllAnimations() {
+        //Walking animation
+        Texture texture = Utility.getTextureAsset(defaultSpritePath);
+        TextureRegion[][] textureFrames = TextureRegion.split(texture, FRAME_WIDTH, FRAME_HEIGHT);
+        walkDownFrames = new Array<>(4);
+        walkLeftFrames = new Array<>(4);
+        walkRightFrames = new Array<>(4);
+        walkUpFrames = new Array<>(4);
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                TextureRegion region = textureFrames[i][j];
+                if (region == null) {
+                    Gdx.app.debug(TAG, "Got null animation frame " + i + "," + j);
+                }
+                switch (i) {
+                    case 0:
+                        walkDownFrames.insert(j, region);
+                        break;
+                    case 1:
+                        walkLeftFrames.insert(j, region);
+                        break;
+                    case 2:
+                        walkRightFrames.insert(j, region);
+                        break;
+                    case 3:
+                        walkUpFrames.insert(j, region);
+                        break;
+                }
+            }
+        }
+
+        walkDownAnimation = new Animation(0.25f, walkDownFrames, Animation.PlayMode.LOOP);
+        walkLeftAnimation = new Animation(0.25f, walkLeftFrames, Animation.PlayMode.LOOP);
+        walkRightAnimation = new Animation(0.25f, walkRightFrames, Animation.PlayMode.LOOP);
+        walkUpAnimation = new Animation(0.25f, walkUpFrames, Animation.PlayMode.LOOP);
+    }
+
+    public void dispose() {
+        Utility.unloadAsset(defaultSpritePath);
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public Sprite getFrameSprite() {
+        return frameSprite;
+    }
+
+    public TextureRegion getFrame() {
+        return currentFrame;
+    }
+
+    public Vector2 getCurrentPosition() {
+        return currentPlayerPosition;
+    }
+
+    public void setCurrentPosition(float currentPositionX, float currentPositionY) {
+        frameSprite.setX(currentPositionX);
+        frameSprite.setY(currentPositionY);
+        this.currentPlayerPosition.x = currentPositionX;
+        this.currentPlayerPosition.y = currentPositionY;
+    }
+
+    public void setDirection(Direction direction, float delta) {
+
+        this.previousDirection = this.currentDirection;
+        this.currentDirection = direction;
+
+        //Look into the appropriate variable when changing position
+
+        switch (currentDirection) {
+            case DOWN:
+                currentFrame = (TextureRegion) walkDownAnimation.getKeyFrame(frameTime);
+                break;
+            case LEFT:
+                currentFrame = (TextureRegion) walkLeftAnimation.getKeyFrame(frameTime);
+                break;
+            case UP:
+                currentFrame = (TextureRegion) walkUpAnimation.getKeyFrame(frameTime);
+                break;
+            case RIGHT:
+                currentFrame = (TextureRegion) walkRightAnimation.getKeyFrame(frameTime);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void setNextPositionToCurrent() {
+        setCurrentPosition(nextPlayerPosition.x, nextPlayerPosition.y);
+    }
+
+    public void calculateNextPosition(Direction currentDirection, float deltaTime) {
+
+        float testX = currentPlayerPosition.x;
+        float testY = currentPlayerPosition.y;
+
+        velocity.scl(deltaTime);
+
+        switch (currentDirection) {
+            case LEFT:
+                testX -= velocity.x;
+                break;
+            case RIGHT:
+                testX += velocity.x;
+                break;
+            case UP:
+                testY += velocity.y;
+                break;
+            case DOWN:
+                testY -= velocity.y;
+                break;
+            default:
+                break;
+        }
+
+        nextPlayerPosition.x = testX;
+        nextPlayerPosition.y = testY;
+
+        //velocity
+        velocity.scl(1 / deltaTime);
+    }
+}
--- core/src/main/java/org/seariver/rpg/screens/MainGameScreen.java
+++ core/src/main/java/org/seariver/rpg/screens/MainGameScreen.java
@@ -1,17 +1,101 @@
 package org.seariver.rpg.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import org.seariver.rpg.Entity;
+import org.seariver.rpg.MapManager;
+import org.seariver.rpg.PlayerController;
 
 public class MainGameScreen implements Screen {
 
+    private static final String TAG = MainGameScreen.class.getSimpleName();
+
+    private static class VIEWPORT {
+        static float viewportWidth;
+        static float viewportHeight;
+        static float virtualWidth;
+        static float virtualHeight;
+        static float physicalWidth;
+        static float physicalHeight;
+        static float aspectRatio;
+    }
+
+    private static MapManager mapMgr;
+    private static Entity player;
+
+    private PlayerController controller;
+    private TextureRegion currentPlayerFrame;
+    private Sprite currentPlayerSprite;
+    private OrthogonalTiledMapRenderer mapRenderer;
+    private OrthographicCamera camera;
+
+    public MainGameScreen() {
+        mapMgr = new MapManager();
+    }
+
     @Override
     public void show() {
+        //camera setup
+        setupViewport(10, 10);
+
+        //get the current size
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, VIEWPORT.viewportWidth, VIEWPORT.viewportHeight);
+
+        mapRenderer = new OrthogonalTiledMapRenderer(mapMgr.getCurrentMap(), MapManager.UNIT_SCALE);
+        mapRenderer.setView(camera);
+
+        Gdx.app.debug(TAG, "UnitScale value is: " + mapRenderer.getUnitScale());
+
+        player = new Entity();
+        player.init(mapMgr.getPlayerStartUnitScaled().x, mapMgr.getPlayerStartUnitScaled().y);
+
+        currentPlayerSprite = player.getFrameSprite();
+
+        controller = new PlayerController(player);
+        Gdx.input.setInputProcessor(controller);
+    }
+
+    @Override
+    public void hide() {
 
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        //Preferable to lock and center the camera to the player’s position
+        camera.position.set(currentPlayerSprite.getX(), currentPlayerSprite.getY(), 0f);
+        camera.update();
+
+        player.update(delta);
+        currentPlayerFrame = player.getFrame();
+
+        updatePortalLayerActivation(player.boundingBox);
+
+        if (!isCollisionWithMapLayer(player.boundingBox)) {
+            player.setNextPositionToCurrent();
+        }
+
+        controller.update(delta);
+
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+
+        mapRenderer.getBatch().begin();
+        mapRenderer.getBatch().draw(currentPlayerFrame, currentPlayerSprite.getX(), currentPlayerSprite.getY(), 1, 1);
+        mapRenderer.getBatch().end();
     }
 
     @Override
@@ -30,12 +114,97 @@ public class MainGameScreen implements Screen {
     }
 
     @Override
-    public void hide() {
-
+    public void dispose() {
+        controller.dispose();
+        Gdx.input.setInputProcessor(null);
+        mapRenderer.dispose();
     }
 
-    @Override
-    public void dispose() {
+    private void setupViewport(int width, int height) {
+        //Make the viewport a percentage of the total display area
+        VIEWPORT.virtualWidth = width;
+        VIEWPORT.virtualHeight = height;
 
+        //Current viewport dimensions
+        VIEWPORT.viewportWidth = VIEWPORT.virtualWidth;
+        VIEWPORT.viewportHeight = VIEWPORT.virtualHeight;
+
+        //pixel dimensions of display
+        VIEWPORT.physicalWidth = Gdx.graphics.getWidth();
+        VIEWPORT.physicalHeight = Gdx.graphics.getHeight();
+
+        //aspect ratio for current viewport
+        VIEWPORT.aspectRatio = (VIEWPORT.virtualWidth / VIEWPORT.virtualHeight);
+
+        //update viewport if there could be skewing
+        if (VIEWPORT.physicalWidth / VIEWPORT.physicalHeight >= VIEWPORT.aspectRatio) {
+            //Letterbox left and right
+            VIEWPORT.viewportWidth = VIEWPORT.viewportHeight * (VIEWPORT.physicalWidth / VIEWPORT.physicalHeight);
+            VIEWPORT.viewportHeight = VIEWPORT.virtualHeight;
+        } else {
+            //letterbox above and below
+            VIEWPORT.viewportWidth = VIEWPORT.virtualWidth;
+            VIEWPORT.viewportHeight = VIEWPORT.viewportWidth * (VIEWPORT.physicalHeight / VIEWPORT.physicalWidth);
+        }
+
+        Gdx.app.debug(TAG, "WorldRenderer: virtual: (" + VIEWPORT.virtualWidth + "," + VIEWPORT.virtualHeight + ")");
+        Gdx.app.debug(TAG, "WorldRenderer: viewport: (" + VIEWPORT.viewportWidth + "," + VIEWPORT.viewportHeight + ")");
+        Gdx.app.debug(TAG, "WorldRenderer: physical: (" + VIEWPORT.physicalWidth + "," + VIEWPORT.physicalHeight + ")");
+    }
+
+    private boolean isCollisionWithMapLayer(Rectangle boundingBox) {
+
+        MapLayer mapCollisionLayer = mapMgr.getCollisionLayer();
+
+        if (mapCollisionLayer == null) {
+            return false;
+        }
+
+        Rectangle rectangle = null;
+
+        for (MapObject object : mapCollisionLayer.getObjects()) {
+            if (object instanceof RectangleMapObject) {
+                rectangle = ((RectangleMapObject) object).getRectangle();
+                if (boundingBox.overlaps(rectangle)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean updatePortalLayerActivation(Rectangle boundingBox) {
+
+        MapLayer mapPortalLayer = mapMgr.getPortalLayer();
+
+        if (mapPortalLayer == null) {
+            return false;
+        }
+
+        Rectangle rectangle = null;
+
+        for (MapObject object : mapPortalLayer.getObjects()) {
+            if (object instanceof RectangleMapObject) {
+                rectangle = ((RectangleMapObject) object).getRectangle();
+                if (boundingBox.overlaps(rectangle)) {
+                    String mapName = object.getName();
+
+                    if (mapName == null) {
+                        return false;
+                    }
+
+                    mapMgr.setClosestStartPositionFromScaledUnits(player.getCurrentPosition());
+                    mapMgr.loadMap(mapName);
+                    player.init(mapMgr.getPlayerStartUnitScaled().x, mapMgr.getPlayerStartUnitScaled().y);
+                    mapRenderer.setMap(mapMgr.getCurrentMap());
+                    Gdx.app.debug(TAG, "Portal Activated");
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
